33ba61ddfe66f31330c5ce13ddd286bea92fee72
Kamil Molwani <kmolwani93@gmail.com>
2018-07-13 18:03:13 -0700

    Decreased restarting time after winning

--- js/app.js
+++ js/app.js
@@ -312,7 +312,7 @@ function level() {
     var winningMessage = document.createElement('h1')
     document.body.appendChild(winningMessage);
     winningMessage.innerHTML = "<pre>" + "Congratulations!!! \n\n You have WON the game." + "</pre>";
-    setTimeout(function() {location.reload()}, 3000);
+    setTimeout(function() {location.reload()}, 1500);
   }
   //using pre tag to be able to use new-line string
   var div = document.getElementById('level');
